2cd476fbb670e7b7a388f23ca5bde368ce2256b8
Gdhfhtgbyx <48551808+Gdhfhtgbyx@users.noreply.github.com>
2019-06-05 23:32:19 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -826,4 +826,4 @@ async def embed(ctx, *args):
     await client.delete_message(ctx.message)
 
 
-client.run('NTU1NzIxMjk2MzcwMDczNjAx.XPhOMA.Cu3WjujTbaekyhv4Lp6srK33zNo')
+client.run(os.getenv('NTU1NzIxMjk2MzcwMDczNjAx.XPhOMA.Cu3WjujTbaekyhv4Lp6srK33zNo'))
