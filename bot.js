dc36dde258dabdcfea398c4ee6fc51ea88f70579
Gdhfhtgbyx <48551808+Gdhfhtgbyx@users.noreply.github.com>
2019-06-18 18:41:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,8 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
-const botconfig = require("../botconfig.json");
-const red = botconfig.red;
-const green = botconfig.green;
-const orange = botconfig.orange;
-const errors = require("../utils/errors.js");
+
  
-{
-  "prefix": "!"
-}
 
 client.on('ready', () => {
 
@@ -28,49 +22,6 @@ client.on('message', message => {
 
 });
 
- 
-module.exports.run = async (bot, message, args) => {
-    message.delete();
-    if(args[0] == "help"){
-      message.reply("Usage: !report <user> <reason>");
-      return;
-    }
-    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!rUser) return errors.cantfindUser(message.channel);
-    let rreason = args.join(" ").slice(22);
-    if(!rreason) return errors.noReason(message.channel);
-
-    let reportEmbed = new Discord.RichEmbed()
-    .setDescription("Reports")
-    .setColor(orange)
-    .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
-    .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
-    .addField("Channel", message.channel)
-    .addField("Time", message.createdAt)
-    .addField("Reason", rreason);
-
-    let reportschannel = message.guild.channels.find(`name`, "reports");
-    if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
-    reportschannel.send(reportEmbed);
-
-}
-
-module.exports.help = {
-  name: "report"
-}
-
-module.exports.run = async (bot, message, args) => {
-
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
-  if(!args[0]) return message.channel.send("oof");
-  message.channel.bulkDelete(args[0]).then(() => {
-    message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
-  });
-}
-
-module.exports.help = {
-  name: "clear"
-}
  
 
 // THIS  MUST  BE  THIS  WAY
