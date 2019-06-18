d122d7b3ed76ab761ddccc83b26f8117726d4ecc
Gdhfhtgbyx <48551808+Gdhfhtgbyx@users.noreply.github.com>
2019-06-18 18:33:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,7 +56,20 @@ module.exports.help = {
   name: "report"
 }
 
+module.exports.run = async (bot, message, args) => {
+
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+  if(!args[0]) return message.channel.send("oof");
+  message.channel.bulkDelete(args[0]).then(() => {
+    message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
+  });
+}
+
+module.exports.help = {
+  name: "clear"
+}
  
+
 // THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
