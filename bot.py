7d864c3cd819ee3515770ed49fabe77b11dd059f
Gdhfhtgbyx <48551808+Gdhfhtgbyx@users.noreply.github.com>
2019-06-19 19:44:32 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,9 +20,7 @@ async def status_task():
         await asyncio.sleep(5)
         await client.change_presence(game=discord.Game(name='in '+str(len(client.servers))+' servers'))
         await asyncio.sleep(5)
-        
 
-	
 
 @client.command(pass_context=True)
 async def play (ctx, url):
