e135debf219ac39fae4f322c67939882b460f1df
Gdhfhtgbyx <48551808+Gdhfhtgbyx@users.noreply.github.com>
2019-06-19 19:47:57 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
+from discord.ext.commands.cooldowns import BucketType
 import asyncio
-import random
 import requests
 import os
-
+import platform
+import colorsys
+import random
+import time
+from discord.voice_client import VoiceClient
+from discord import Game, Embed, Color, Status, ChannelType
 
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
 client = Bot(description="DarkBot Bot is best", command_prefix="d!", pm_help = True)
