d3b98ac26d90c739bd7de0718a6897dce5d2fd0e
Gdhfhtgbyx <48551808+Gdhfhtgbyx@users.noreply.github.com>
2019-07-09 19:27:12 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,68 +1,29 @@
-import discord
-from discord.ext import commands
-from discord.ext.commands import Bot
-from discord.ext.commands.cooldowns import BucketType
-import asyncio
-import requests
-import os
-import platform
-import colorsys
-import random
-import time
-from discord.voice_client import VoiceClient
-from discord import Game, Embed, Color, Status, ChannelType
+const Discord = require('discord.js');
 
+const client = new Discord.Client();
 
-Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
-client = Bot(description="DarkBot Bot is best", command_prefix="d!", pm_help = True)
-client.remove_command('help')
+ 
 
+client.on('ready', () => { // know if our bot is online
 
-async def status_task():
-    while True:
-        await client.change_presence(game=discord.Game(name='for d!help'))
-        await asyncio.sleep(5)
-        await client.change_presence(game=discord.Game(name='with '+str(len(set(client.get_all_members())))+' users'))
-        await asyncio.sleep(5)
-        await client.change_presence(game=discord.Game(name='in '+str(len(client.servers))+' servers'))
-        await asyncio.sleep(5)
+    console.log('I am ready!');
 
+});
 
-@client.command(pass_context=True)
-async def play (ctx, url):
-    channel = ctx.message.author.voice_channel
-    await client.join_voice_channel(channel)
-    server = ctx.message.server
-    voice = client.voice_client_in(server)
-    player = await voice.create_ytdl_player(url)
-    player_dict[server.id] = player
-    await client.send_message(ctx.message.channel, "Playing `%s` now" % player.title)
-    player.start()
+ 
 
+client.on('message', message => { // When the message a message is executed
 
-@client.command(pass_context=True)
-async def stop(ctx):
-    server = ctx.message.server
-    player = player_dict[server.id]
-    player.stop()
-    await client.send_message(ctx.message.channel, "Stopped `%s`" % player.title)
-    del player_dict[server.id]
+    if (message.content === 'ping') { // if that message matches ping
 
+       message.reply('pong'); // 
 
-@client.command(pass_context=True)
-async def pause(ctx):
-    server = ctx.message.server
-    player = player_dict[server.id]
-    player.pause()
-    await client.send_message(ctx.message.channel, "Paused `%s`" % player.title)
+       }
 
+});
 
-@client.command(pass_context=True)
-async def resume(ctx):
-    server = ctx.message.server
-    player = player_dict[server.id]
-    player.resume()
-    await client.send_message(ctx.message.channel, "Resumed `%s`" % player.title)
+ 
 
+// THIS  MUST  BE  THIS  WAY
 
-client.run(str(os.environ.get('BOT_TOKEN')))
+client.login(process.env.BOT_TOKEN);//where 
